dbe8c3cbe6672eb44fe6cc577853db8864466ec7
ggg05158 <csg05158@naver.com>
2020-07-29 20:32:39 +0900

    완전제곱수

---
+++ 1977.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+bool divisor(int num)
+{
+    int count=0;
+    for(int i=1;i<=num;i++)
+    {
+        if(num%i==0)
+            count++;
+    }
+    if(count%2==1)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    int M,N,sum=0;
+    cin>>M;
+    cin>>N;
+    int min=N+1;
+    for(int i=N;i>=M;i--)
+    {
+        if(divisor(i)==true)
+        {
+            sum+=i;
+            min=i;
+        }
+    }
+    if(min==N+1)
+        cout<<"1"<<endl;
+    else{
+        cout<<sum<<endl;
+        cout<<min<<endl;
+    }
+    return 0;
+}
