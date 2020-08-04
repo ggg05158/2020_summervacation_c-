8bfe5b592092e74b3c690fde225befb95ed51711
ggg05158 <csg05158@naver.com>
2020-08-04 22:22:16 +0900

    가장비싼선수

--- 11098.cpp
+++ 11098.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+class testCase{
+public:
+    int price;
+    string name;
+};
+int main()
+{
+    int time1,time2,high=0;
+    cin>>time1;
+    for(int i=0;i<time1;i++)
+    {
+        cin>>time2;
+        testCase *array=new testCase[time2];
+        for(int j=0;j<time2;j++)
+        {
+            cin >>array[j].price>>array[j].name;
+            high=(array[high].price>=array[j].price)?high:j;
+        }
+        cout<<array[high].name;
+        delete[] array;
+    }
+    return 0;  
+}
