afa44679f5a47415ec20dce40d6c214ca683ad81
ggg05158 <csg05158@naver.com>
2020-08-06 20:02:15 +0900

    별 피라미드 13

---
+++ 2523.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++)
+	{
+		for (int j = 0; j < i; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	for (int i = 1; i <= T - 1; i++)
+	{
+		for (int j = 0; j < T - i; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	return 0;
+}
