55a279434a6cf4d830746a87a06d9a7bc9a3ac51
ggg05158 <csg05158@naver.com>
2020-08-06 17:46:01 +0900

    A+B 3

---
+++ 10950.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int T,n1,n2;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> n1 >> n2;
+		cout << n1 + n2<<"\n";
+	}
+	return 0;
+}
